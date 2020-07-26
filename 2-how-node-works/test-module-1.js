76949a89baa50deba4ba45cb1fb40041156ef636
TLChipperfield <tlchipperfield@me.com>
2020-07-26 11:22:01 -0400

    End_Section_4

---
+++ 2-how-node-works/test-module-1.js
@@ -0,0 +1,27 @@
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multipy(a, b) {
+//     return a * b;
+//   }
+
+//   devid(a, b) {
+//     return a / b;
+//   }
+// }
+
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  devid(a, b) {
+    return a / b;
+  }
+};
