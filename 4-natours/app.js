4875a5c5d169d793bfaca79318b32e709daa8378
TLChipperfield <tlchipperfield@me.com>
2020-08-16 16:56:35 -0400

    end_lecture_54

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -19,6 +19,26 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
