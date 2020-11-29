96cfdd255661ab9df653bc09af959403daa2985b
TLChipperfield <tlchipperfield@me.com>
2020-11-29 18:58:47 -0500

    end_lecture_67

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,6 +7,10 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Middlewares
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
