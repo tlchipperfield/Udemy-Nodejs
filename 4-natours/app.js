00066d6e1f05d9cf9dad876f74ef717bda75017f
TLChipperfield <tlchipperfield@me.com>
2020-11-29 06:07:41 -0500

    end_lecture_60

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) Middlewares
+app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware :)');
@@ -26,6 +30,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) Route Handlers
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -115,6 +120,7 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -123,6 +129,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// 4) Start Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
