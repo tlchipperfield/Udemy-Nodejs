12333745f97c7d047f9fa6fc41b6569a6a9ce42b
TLChipperfield <tlchipperfield@me.com>
2020-11-29 06:19:17 -0500

    end_lecture_61

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,6 +7,7 @@ const app = express();
 // 1) Middlewares
 app.use(morgan('tiny'));
 
+app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from the middleware :)');
   next();
@@ -114,6 +115,36 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
@@ -128,6 +159,14 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // 4) Start Server
 const port = 3000;
