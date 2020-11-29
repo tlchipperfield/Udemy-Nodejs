5a92296c5014da454fc5f962771989076d6ea83f
TLChipperfield <tlchipperfield@me.com>
2020-11-29 18:23:11 -0500

    end_lecture_65

--- 4-natours/Routes/tourRoutes.js
+++ 4-natours/Routes/tourRoutes.js
@@ -5,10 +5,16 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and the price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
