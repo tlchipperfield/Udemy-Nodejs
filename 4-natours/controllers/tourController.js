c0d735a1a25717f1e7164d286bf83412816f9040
TLChipperfield <tlchipperfield@me.com>
2020-12-28 14:03:59 -0500

    end_lecture_88

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -26,13 +26,25 @@ exports.getTour = (req, res) => {
   //   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    //   },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
