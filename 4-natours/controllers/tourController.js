4f7feae2481330f3984a0b7f4175311b548f18f5
TLChipperfield <tlchipperfield@me.com>
2020-12-31 06:13:51 -0500

    end_lecture_94

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,8 +1,24 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  const tours = await Tour.find();
   try {
+    // Build the Query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // Execute the query
+    const tours = await query;
+
+    // Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
