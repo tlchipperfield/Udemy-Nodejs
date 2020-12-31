00a13d5f016c02f180e5e63289332c031feee5b6
TLChipperfield <tlchipperfield@me.com>
2020-12-31 09:01:22 -0500

    end_lecture_98

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -32,15 +32,22 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, etc...
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
     // Execute the query
     const tours = await query;
 
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-    // Send response
+    // Send the respose
     res.status(200).json({
       status: 'success',
       results: tours.length,
