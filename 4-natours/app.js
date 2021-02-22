4fe5a1cc2796a94ac28bba9ae42e17aadf798478
TLChipperfield <tlchipperfield@me.com>
2021-02-22 09:59:51 -0500

    end_143

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,15 +10,27 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) Middlewares
+// 1) Global Middlewares
+// Set security HTTP header
+app.use(helmet())
+// Devlopment logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit req from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
-app.use(morgan('dev'));
-app.use(express.json());
+// Body Parse, reading data from boyd into req.body
+app.use(express.json({ limit: '10kb' }));
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
