245a10e74a5a5338d12aac226465e9950dc434b5
TLChipperfield <tlchipperfield@me.com>
2021-02-22 09:39:20 -0500

    end_141

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -5,6 +5,7 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
+const { now } = require('mongoose');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -14,7 +15,19 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
+  res.cookie('jwt', token, cookieOptions);
+
+  // remove password from cookie
+  user.password = undefined;
+  
   res.status(statusCode).json({
     status: 'success',
     token,
