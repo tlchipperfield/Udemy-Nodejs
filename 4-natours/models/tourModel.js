a488f1da934c97200eb922ce49b399cc7aec06b2
TLChipperfield <tlchipperfield@me.com>
2021-03-14 21:50:56 -0400

    end_171

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -175,10 +175,10 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
