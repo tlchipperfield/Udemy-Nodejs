ce07d2fef46000efd5bef597e3c1dff63ac5ff09
TLChipperfield <tlchipperfield@me.com>
2021-05-31 19:55:10 -0400

    end_188

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,6 +40,7 @@ app.use('/api', limiter);
 
 // Body Parse, reading data from boyd into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -63,6 +65,7 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
